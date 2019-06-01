8e8f09ddfba1a9e8b6482dee3493f47f78cb18db
Thomas2124 <40778004+Thomas2124@users.noreply.github.com>
2019-06-01 23:04:19 +1000

    setting up battle scene

---
+++ 19T2Studio3Group4/Assets/Scripts/Thomas Scripts/Character.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float health = 100f;
+    public float attackRating = 10f;
+    public float defenceRating = 10f;
+    public float specialBar = 0f;
+    public int level = 1;
+    public float exp = 0f;
+    public bool isDead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void Defence()
+    {
+
+    }
+
+    public void Special()
+    {
+
+    }
+
+    public void TakeDamage()
+    {
+
+    }
+
+    public void GainExp()
+    {
+
+    }
+
+    public void GainLevel()
+    {
+
+    }
+}
---
+++ 19T2Studio3Group4/Assets/Scripts/Thomas Scripts/Player1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public GameObject[] characters;
+    public int orderNumber = 1;
+    public bool isMyTurn = false;
+    public bool isDefeated = false;
+    public GameObject activeCharacter;
+    public GameObject opponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GameOver()
+    {
+
+    }
+}
