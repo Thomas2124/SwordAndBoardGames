087d04cab8b4c349993ea663d40bc19ca9c878d5
Thomas L <tlrocks99@gmail.com>
2019-06-14 11:02:14 +1000

    add new drag and drop save, child spawn button

---
+++ 19T2Studio3Group4/Assets/Scripts/Thomas Scripts/Saveload scripts/PlaceChecker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceChecker : MonoBehaviour
+{
+    public GameObject place1;
+    public GameObject place2;
+    public GameObject place3;
+
+    public GameObject firstCharacter;
+    public GameObject secondCharacter;
+    public GameObject thirdCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        place1 = GameObject.Find("CharacterSlot1.image");
+        place2 = GameObject.Find("CharacterSlot2.image");
+        place3 = GameObject.Find("CharacterSlot3.image");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (place1.transform.childCount > 0)
+        {
+            firstCharacter = place1.transform.GetChild(0).gameObject;
+        }
+
+        if (place2.transform.childCount > 0)
+        {
+            secondCharacter = place2.transform.GetChild(0).gameObject;
+        }
+
+        if (place2.transform.childCount > 0)
+        {
+            thirdCharacter = place3.transform.GetChild(0).gameObject;
+        }
+    }
+}
