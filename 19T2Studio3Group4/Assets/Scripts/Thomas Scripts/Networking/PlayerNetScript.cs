5d5dc9157c9bef89600d162dd68e40022c382661
Pandaoil <40745595+Pandaoil@users.noreply.github.com>
2019-06-17 10:30:59 +1000

    Revert "Revert "Fixed Merge""
    This reverts commit e1dc86490128ed9793ec0166741ef1fb2c5fba5f.

---
+++ 19T2Studio3Group4/Assets/Scripts/Thomas Scripts/Networking/PlayerNetScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PlayerNetScript : NetworkBehaviour
+{
+    //public Text myPlayerText;
+    //public Button myNextButton;
+    //public bool isPlayer1 = false;
+    //public bool isPlayer2 = false;
+
+    [SyncVar]
+    public Text myPlayerText;
+    public Button myNextButton;
+    public string PlayerName = "player";
+    public bool isPlayer1 = false;
+    public bool isPlayer2 = false;
+
+    [Command]
+    void CmdChangeName(string name)
+    {
+        PlayerName = name;
+        myPlayerText.text = PlayerName;
+        RpcOnChangePlayerName(name);
+    }
+
+    [ClientRpc]
+    void RpcOnChangePlayerName(string n)
+    {
+        PlayerName = n;
+        myPlayerText.text = PlayerName;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer == true)
+        {
+            DummyManager script = GameObject.Find("GameManager").GetComponent<DummyManager>();
+            if (script.player1Joined == false)
+            {
+                script.player1Joined = true;
+                isPlayer1 = true;
+                isPlayer2 = false;
+                PlayerName = "Bob";
+                CmdChangeName(PlayerName);
+                myNextButton.transform.position = new Vector2(myNextButton.transform.position.x, myNextButton.transform.position.y + 200f);
+            }
+            else if (script.player1Joined == true)
+            {
+                script.player2Joined = true;
+                isPlayer1 = false;
+                isPlayer2 = true;
+                PlayerName = "Mike";
+                CmdChangeName(PlayerName);
+                myNextButton.transform.position = new Vector2(myNextButton.transform.position.x, myNextButton.transform.position.y - 50f);
+            }
+            else if(script.player1Joined == true && script.player2Joined == true)
+            {
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 19T2Studio3Group4/Assets/Scripts/Thomas Scripts/ShaderScripts/NewShader.shader
@@ -0,0 +1,46 @@
+﻿Shader "Custom/NewShader"
+{
+	//stuff that can be seen in the inspector
+	Properties
+	{
+		_Color("Tint", Color) = (0, 0, 0, 1) //Main colour of the texture
+		_MainTex("Texture", 2D) = "white" {} //Texture
+		_Smoothness("Smoothness", Range(0, 1)) = 0 //Roughness of the surface
+		_Metallic("Metalness", Range(0, 1)) = 0 //if you want it to look like metal or not
+		[HDR] _Emission("Emission", Color) = (0, 0, 0, 1) //if you want it to glow
+	}
+	SubShader
+	{
+		Tags{ "RenderType" = "Opaque" "Queue" = "Geometry"}
+
+		CGPROGRAM
+
+		#pragma surface surf Standard fullforwardshadows //#pragma (what kind of shader) (name of method) (what kind of lighting model)
+		#pragma target 3.0 //uses higher precision values for better lighting and shadows
+
+		sampler2D _MainTex;
+		fixed4 _Color;
+		half _Smoothness;
+		half _Metallic;
+		half3 _Emission;
+
+		//Information that is needed to set the colour of the texture
+		struct Input 
+		{
+			float2 uv_MainTex; //UV coordinates
+		};
+
+		//Method for surface related stuff
+		void surf(Input i, inout SurfaceOutputStandard o) 
+		{
+			fixed4 col = tex2D(_MainTex, i.uv_MainTex);
+			col *= _Color;
+			o.Albedo = col.rgb;
+			o.Metallic = _Metallic;
+			o.Smoothness = _Smoothness;
+			o.Emission = _Emission;
+		}
+		ENDCG
+	}
+	FallBack "Standard" //"Standard" create shadows on other surfaces
+}
