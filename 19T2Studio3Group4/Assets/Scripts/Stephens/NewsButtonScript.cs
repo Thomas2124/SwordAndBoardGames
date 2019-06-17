ef5c387662415b78a2aa6b706e27ef13aea2a81f
StephMcleod <40745594+StephMcleod@users.noreply.github.com>
2019-06-17 10:32:23 +1000

    creatingButtonScript

---
+++ 19T2Studio3Group4/Assets/Scripts/Stephens/NewsButtonScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewsButtonScript : MonoBehaviour
+{
+	public GameObject shooop;
+	public GameObject shopOptionOne;
+	public GameObject shopOption;
+	
+	public void OpenShopAdd()
+	{	
+		shooop.SetActive (true);
+		shopOptionOne.SetActive (true);
+		shopOption.SetActive (true);
+	}
+}
