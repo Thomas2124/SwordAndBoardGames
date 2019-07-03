16a53d4ed0799bd5fbddb9467f17d435580d3027
StephMcleod <40745594+StephMcleod@users.noreply.github.com>
2019-07-03 11:45:31 +0100

    SceneTransferScript

---
+++ Assets/Scripts/SceneTransferScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransferScript : MonoBehaviour
+{
+	public string nameOfScene;
+	
+	public void SceneSwitch()
+	{
+		SceneManager.LoadScene(nameOfScene);
+	}
+}
