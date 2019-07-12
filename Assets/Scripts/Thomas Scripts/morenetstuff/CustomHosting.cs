ba03d911008e06205163f8f5054de9f381125ff4
Thomas L <tlrocks99@gmail.com>
2019-07-12 11:39:42 +1000

    started Custom online matchmaking UI

--- Assets/Scripts/Thomas Scripts/morenetstuff/CustomHosting.cs
+++ Assets/Scripts/Thomas Scripts/morenetstuff/CustomHosting.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.Networking.Types;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class CustomHosting : NetworkManager
 {
+    NetworkID netId;
     public void SetupHost()
     {
         SetPort();
@@ -20,6 +23,34 @@ public class CustomHosting : NetworkManager
         NetworkManager.singleton.StartClient();
     }
 
+    public void SetupMatchMakingHost()
+    {
+        NetworkManager.singleton.StartMatchMaker();
+        NetworkManager.singleton.matchMaker.ListMatches(0, 10, "", true, 0, 0, OnMatchList);
+        string ipAddress = GameObject.Find("InputFieldIPAddress").transform.Find("Text").GetComponent<Text>().text;
+
+        NetworkManager.singleton.matchMaker.CreateMatch(ipAddress, NetworkManager.singleton.matchSize, true, "", "", "", 0, 0, NetworkManager.singleton.OnMatchCreate);
+
+    }
+
+    public void JoinMatchMakingGame()
+    {
+        NetworkManager.singleton.StartMatchMaker();
+        NetworkManager.singleton.matchMaker.ListMatches(0, 10, "", true, 0, 0, OnMatchList);
+        if (NetworkManager.singleton.matches != null)
+        {
+            string ipAddress = GameObject.Find("InputFieldIPAddress").transform.Find("Text").GetComponent<Text>().text;
+            for (int i = 0; i < NetworkManager.singleton.matches.Count; i++)
+            {
+                var match = NetworkManager.singleton.matches[i];
+                if (match.name == ipAddress)
+                {
+                    NetworkManager.singleton.matchName = ipAddress;
+                    NetworkManager.singleton.matchMaker.JoinMatch(netId, "", "", "", 0, 0, NetworkManager.singleton.OnMatchJoined);
+                }
+            }
+        }
+    }
 
     public void BackToMenuButton()
     {
@@ -57,6 +88,12 @@ public class CustomHosting : NetworkManager
 
         GameObject.Find("JoinButton").GetComponent<Button>().onClick.RemoveAllListeners();
         GameObject.Find("JoinButton").GetComponent<Button>().onClick.AddListener(this.JoinGame);
+
+        GameObject.Find("MMHostButton").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("MMHostButton").GetComponent<Button>().onClick.AddListener(this.SetupMatchMakingHost);
+
+        GameObject.Find("MMJoinButton").GetComponent<Button>().onClick.RemoveAllListeners();
+        GameObject.Find("MMJoinButton").GetComponent<Button>().onClick.AddListener(this.JoinMatchMakingGame);
 
         GameObject.Find("BackButton").GetComponent<Button>().onClick.RemoveAllListeners();
         GameObject.Find("BackButton").GetComponent<Button>().onClick.AddListener(this.BackToMenuButton);
