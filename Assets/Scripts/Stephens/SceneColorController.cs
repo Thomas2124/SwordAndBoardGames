1bd70324fc21c2cc6c8dce9930e3ee91be9da2cd
StephMcleod <40745594+StephMcleod@users.noreply.github.com>
2019-07-28 15:52:47 +0100

    u; image update 1

---
+++ Assets/Scripts/Stephens/SceneColorController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneColorController : MonoBehaviour
+{
+	public GameObject colorReference;
+	public GameObject screenObject;
+	public static bool screenObjectIsVisable;
+	
+	public void White()
+	{
+		colorReference.GetComponent<Image>().color = new Color(0, 0, 0);
+	}
+	public void Black()
+	{
+		colorReference.GetComponent<Image>().color = new Color(255, 255, 255);
+	}
+	public void Red()
+	{
+		colorReference.GetComponent<Image>().color = new Color(255, 0, 0);
+	}
+	public void Blue()
+	{
+		colorReference.GetComponent<Image>().color = new Color(0, 0, 255);
+	}
+	public void Green()
+	{
+		colorReference.GetComponent<Image>().color = new Color(0, 255, 0);
+	}
+	public void Purple()
+	{
+		colorReference.GetComponent<Image>().color = new Color(255, 0, 255);
+	}
+	public void Yellow()
+	{
+		colorReference.GetComponent<Image>().color = new Color(0, 255, 255);
+	}
+	public void Orange()
+	{
+		colorReference.GetComponent<Image>().color = new Color(255, 255, 0);
+	}
+	public void ScreenSwitch(){
+		if (menuIsVisable)
+		{
+			CloseScreen();
+		}
+		else
+		{
+			ScreenVisable();
+		}
+	}
+	
+	void CloseScreen(){
+		screenObject.SetActive (false);
+		screenObjectIsVisable = false;
+	}
+	
+	void ScreenVisable(){
+		screenObject.SetActive (true);
+		screenObjectIsVisable = true;
+	}		
+}
