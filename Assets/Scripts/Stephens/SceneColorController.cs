ae148494548f673c288642cc719370808c6bc1e8
StephMcleod <40745594+StephMcleod@users.noreply.github.com>
2019-08-05 07:03:04 +0100

    Updated Color Picker Progress

--- Assets/Scripts/Stephens/SceneColorController.cs
+++ Assets/Scripts/Stephens/SceneColorController.cs
@@ -24,38 +24,38 @@ public class SceneColorController : MonoBehaviour
         }
     }
 
-    public void White()
-	{
-		colorReference.GetComponent<Image>().color = new Color(0, 0, 0);
-	}
-	public void Black()
-	{
-		colorReference.GetComponent<Image>().color = new Color(255, 255, 255);
-	}
-	public void Red()
-	{
-		colorReference.GetComponent<Image>().color = new Color(255, 0, 0);
-	}
-	public void Blue()
-	{
-		colorReference.GetComponent<Image>().color = new Color(0, 0, 255);
-	}
-	public void Green()
-	{
-		colorReference.GetComponent<Image>().color = new Color(0, 255, 0);
-	}
-	public void Purple()
-	{
-		colorReference.GetComponent<Image>().color = new Color(255, 0, 255);
-	}
-	public void Yellow()
-	{
-		colorReference.GetComponent<Image>().color = new Color(0, 255, 255);
-	}
-	public void Orange()
-	{
-		colorReference.GetComponent<Image>().color = new Color(255, 255, 0);
-	}
+//   public void White()
+//	{
+//		colorReference.GetComponent<Image>().color = new Color(0, 0, 0);
+//	}
+//	public void Black()
+//	{
+//		colorReference.GetComponent<Image>().color = new Color(255, 255, 255);
+//	}
+//	public void Red()
+//	{
+//		colorReference.GetComponent<Image>().color = new Color(255, 0, 0);
+//	}
+//	public void Blue()
+//	{
+//		colorReference.GetComponent<Image>().color = new Color(0, 0, 255);
+//	}
+//	public void Green()
+//	{
+//		colorReference.GetComponent<Image>().color = new Color(0, 255, 0);
+//	}
+//	public void Purple()
+//	{
+//		colorReference.GetComponent<Image>().color = new Color(255, 0, 255);
+//	}
+//	public void Yellow()
+//	{
+//		colorReference.GetComponent<Image>().color = new Color(0, 255, 255);
+//	}
+//	public void Orange()
+//	{
+//		colorReference.GetComponent<Image>().color = new Color(255, 255, 0);
+//	}
 
 	public void ScreenSwitch(){
 		if (screenObjectIsVisable)
